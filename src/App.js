96e376bcbe0adecd4c08c15bd020be929467268a
Ramdhannp <ramdhannp6@gmail.com>
2023-06-07 16:42:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "assets/scss/style.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
